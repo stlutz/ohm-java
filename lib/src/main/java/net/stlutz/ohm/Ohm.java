a7da0e2d6179003d87aa7320e3b4ae98b757a6cd
stlutz <dev@stlutz.net>
2023-03-13 14:13:56 +0100

    Implement construction of grammars from Ohm DSL.

--- lib/src/main/java/net/stlutz/ohm/Ohm.java
+++ lib/src/main/java/net/stlutz/ohm/Ohm.java
@@ -1,13 +1,31 @@
 package net.stlutz.ohm;
 
+import java.util.List;
+
 public final class Ohm {
+  static final BuildGrammar buildGrammarSemantics =
+      Grammar.OhmGrammar.createSemanticsBlueprint(BuildGrammar.class).instantiate();
+
   private Ohm() {}
 
   public static Grammar grammar(String source) {
-    return null;
+    // TODO: assert
+    return grammars(source, null).get(0);
   }
 
-  public static Grammar[] grammars(String source) {
-    return new Grammar[0];
+  public static List<Grammar> grammars(String source) {
+    return grammars(source, null);
+  }
+
+  public static List<Grammar> grammars(String source, Namespace namespace) {
+    if (source == null) {
+      throw new OhmException("source must not be null");
+    }
+    var matchResult = Grammar.OhmGrammar.match(source);
+    if (matchResult.failed()) {
+      throw new OhmException("Syntax error in grammar source");
+    }
+
+    return buildGrammarSemantics.buildGrammars(matchResult.getRootNode(), namespace);
   }
 }
